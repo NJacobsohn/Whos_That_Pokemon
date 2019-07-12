f14cda7c7f9c5666cd27240aa72fe0815019d0bb
Nicolas Jacobsohn <nicolasjacobsohn@Nicolass-MacBook-Pro.local>
2019-07-11 23:03:15 -0600

    Updated class with slightly more robust options

--- src/cnn_class.py
+++ src/cnn_class.py
@@ -3,6 +3,7 @@ from keras.layers import Dense, Dropout, Activation, Flatten, SpatialDropout2D
 from keras.layers import Convolution2D, MaxPooling2D, BatchNormalization, SeparableConv2D
 from keras.layers.convolutional import Conv2D
 from keras.utils import np_utils
+from keras import metrics
 from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import classification_report, confusion_matrix
@@ -16,27 +17,23 @@ class PokemonCNN(object):
     This class is meant to make testing various things on my CNN easier, should involve less swapping between terminals and development environments
     '''
 
-    def __init__(self, train_path, val_path, test_path, model_name=None):
+    def __init__(self, train_path, val_path, test_path, model_name=None, model_type="CNN", weight_path=None):
         '''
         Input:
             Files paths for your train/val/test files (or train/test/holdout, whatever you want to call it, I'm not your mother)
         '''
-        self.n_train = None
-        self.n_val = None
-        self.n_test = None
-        self.model = None
-        self.epochs = None
-        self.batch = None
+        self.weight_path = weight_path
         self.model_name = model_name
         self.model_save_path = "../models/"
         self.metrics_save_path = "../models/metrics/"
         self.train_path = train_path
         self.val_path = val_path
         self.test_path = test_path
-        self.train_gen = None
-        self.val_gen = None
-        self.test_gen = None
-        self.nb_classes = None
+        self.model_type = model_type
+        if self.model_type.lower() == "cnn":
+            self.model_build_function = self.build_cnn_model
+        elif self.model_type.lower() == 'xception':
+            self.model_build_function = self.build_xception_model
         self._len_init()
         self.param_init()
         self.create_generators()
@@ -80,7 +77,7 @@ class PokemonCNN(object):
             print("Saved confusion matrix to \"" + metric_path + "_cm.txt\"")
 
         # Create classification report and save it
-        with open('../pickles/class_names.p', 'rb') as f:
+        with open('../pickles/class_names_gen1_grouped.p', 'rb') as f:
             class_names = np.array(pickle.load(f))
 
         class_report = classification_report(self.test_gen.classes, y_pred, target_names=class_names)
@@ -126,7 +123,7 @@ class PokemonCNN(object):
             shuffle=False)
 
 
-    def build_model(self, kernel_size=(3, 3), pool_size=(2, 2), droupout_perc=0.25, num_blocks=1):
+    def build_cnn_model(self, kernel_size=(3, 3), pool_size=(2, 2), droupout_perc=0.25, num_blocks=1, custom_weights=None):
         '''
         INPUT:
             kernel_size (tuple): set filter size
@@ -145,34 +142,42 @@ class PokemonCNN(object):
 
         self.model.add(Conv2D(self.nb_filters, (kernel_size[0], kernel_size[1]),
                     padding='valid',
-                    input_shape=(self.image_size[0], self.image_size[1], 3))) #first conv. layer
-        self.model.add(Activation('tanh'))
+                    input_shape=(self.image_size[0], self.image_size[1], 3), name="conv1_b1")) #first conv. layer
+        self.model.add(Activation('tanh', name="act1_b1"))
 
-        self.model.add(Conv2D(self.nb_filters, (kernel_size[0], kernel_size[1]), padding='same')) #2nd conv. layer 
-        self.model.add(Activation('tanh'))
+        self.model.add(Conv2D(self.nb_filters, (kernel_size[0], kernel_size[1]), padding='same', name="conv2_b1")) #2nd conv. layer 
+        self.model.add(Activation('tanh', name="act2_b1"))
 
-        self.model.add(MaxPooling2D(pool_size=pool_size)) # decreases size, helps prevent overfitting
-        self.model.add(Dropout(droupout_perc)) # zeros out some fraction of inputs, helps prevent overfitting
+        self.model.add(MaxPooling2D(pool_size=pool_size, name="maxpool_b1")) # decreases size, helps prevent overfitting
+        self.model.add(Dropout(droupout_perc, name="dropout_b1")) # zeros out some fraction of inputs, helps prevent overfitting
 
 
         for block_num in range(num_blocks):
             filter_augmentation = 32 * (block_num + 1)
-            self.model.add(SeparableConv2D(self.nb_filters+filter_augmentation, (kernel_size[0], kernel_size[1]), padding='same')) #3rd conv. layer
-            self.model.add(Activation('tanh'))
-            self.model.add(SeparableConv2D(self.nb_filters+filter_augmentation, (kernel_size[0], kernel_size[1]), padding='same')) #4th conv. layer
-            self.model.add(Activation('tanh'))
-            self.model.add(MaxPooling2D(pool_size=pool_size))
-            self.model.add(Dropout(droupout_perc))
+            self.model.add(SeparableConv2D(self.nb_filters+filter_augmentation, (kernel_size[0], kernel_size[1]), padding='same', name="sepconv1_b{}".format(block_num +2))) #3rd conv. layer
+            self.model.add(Activation('tanh', name="act1_b{}".format(block_num +2)))
+            self.model.add(SeparableConv2D(self.nb_filters+filter_augmentation, (kernel_size[0], kernel_size[1]), padding='same', name="sepconv2_b{}".format(block_num +2))) #4th conv. layer
+            self.model.add(Activation('tanh', name="act2_b{}".format(block_num +2)))
+            self.model.add(MaxPooling2D(pool_size=pool_size, name="maxpool_b{}".format(block_num + 2)))
+            self.model.add(Dropout(droupout_perc, name="dropout_b{}".format(block_num + 2)))
 
         self.model.add(Flatten()) # necessary to flatten before going into conventional dense layer
         print('Model flattened out to ', self.model.output_shape)
 
-        self.model.add(Dense(self.neurons)) # neurons can change
-        self.model.add(Activation('relu'))
+        self.model.add(Dense(self.neurons, name="dense1_blockfinal")) # neurons can change
+        self.model.add(Activation('relu', name="act1_blockfinal"))
 
-        self.model.add(Dense(self.nb_classes)) # 149 final nodes (one for each class)
-        self.model.add(Activation('softmax')) # keep softmax at end to pick between classes 0-148
-        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        self.model.add(Dense(self.nb_classes, name="dense2_blockfinal")) # 149 final nodes (one for each class)
+        self.model.add(Activation('softmax', name="act2_blockfinal")) # keep softmax at end to pick between classes 0-148
+        if custom_weights is not None:
+            self.model.load_weights(self.weight_path, by_name=True)
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', self.top_3_accuracy, 'top_k_categorical_accuracy'])
+
+    def top_3_accuracy(self, y_true, y_pred):
+        return metrics.top_k_categorical_accuracy(y_true, y_pred, k=3)
+
+    def build_xception_model(self):
+        pass
 
     def _len_init(self):
         self.n_train = sum(len(files) for _, _, files in os.walk(self.train_path))  # number of training samples
@@ -207,6 +212,11 @@ class PokemonCNN(object):
         self.model.save(model_path)
         print("Saved model to \"" + model_path + "\"")
 
+    def save_weights(self):
+        model_path = self.model_save_path + self.model_name + "_weights.h5"
+        self.model.save(model_path)
+        print("Saved model weights to \"" + model_path + "\"")
+
     def make_callbacks(self):
         # Initialize tensorboard for monitoring
         tensorboard = callbacks.TensorBoard(log_dir="../models/",
@@ -214,35 +224,37 @@ class PokemonCNN(object):
                                                   write_graph=True, embeddings_freq=0)
 
         # Initialize model checkpoint to save best model
-        self.savename = '../models/' + 'my_cnn_best' + '.hdf5'
+        self.savename = '../models/' + self.model_name + '_best.hdf5'
         mc = callbacks.ModelCheckpoint(self.savename,
                                              monitor='val_loss', verbose=0, save_best_only=True,
                                              save_weights_only=False, mode='auto', period=1)
         self.callbacks = [mc, tensorboard]
     
     def save_history(self, hist):
-        #val_loss, val_acc, loss, acc
-        numpy_loss_history = np.empty(shape=(4, self.epochs))
-        metrics = ["loss", "acc", "val_loss", "val_acc"]
-        for idx, _ in enumerate(numpy_loss_history):
+        metrics = ["loss", "acc", "top_3_accuracy", "top_k_categorical_accuracy",
+                   "val_loss", "val_acc", "val_top_3_accuracy", "val_top_k_categorical_accuracy"]
+        num_metrics = len(metrics)
+        numpy_loss_history = np.empty(shape=(num_metrics, self.epochs))
+        for idx in range(num_metrics):
             numpy_loss_history[idx] = hist.history[metrics[idx]]
         np.savetxt("../models/{}_history.txt".format(self.model_name), numpy_loss_history.T, delimiter=",")
         
 
 if __name__ == "__main__":
 
-    train_path = "../data/train"
-    val_path = "../data/val"
-    test_path = "../data/test"
+    train_path = "../data/gen1/train"
+    val_path = "../data/gen1/val"
+    test_path = "../data/gen1/test"
+    weight_path = "../models/gen1_grouped_test_weights.h5"
 
     print("Creating Class")
-    my_cnn = PokemonCNN(train_path, val_path, test_path)
+    my_cnn = PokemonCNN(train_path, val_path, test_path, model_name="gen1_grouped_3blocks_doubleres", model_type="CNN")#, weight_path=weight_path)
     print("Initializing Parameters")
-    my_cnn.param_init(epochs=10, batch_size=16, image_size=(64, 64), base_filters=16, final_layer_neurons=128)
+    my_cnn.param_init(epochs=100, batch_size=16, image_size=(64, 64), base_filters=16, final_layer_neurons=128)
     print("Creating Generators")
     my_cnn.create_generators(augmentation_strength=0.4)
     print("Building Model")
-    my_cnn.build_model(kernel_size=(3, 3), pool_size=(2, 2), droupout_perc=0.25, num_blocks=1)
+    my_cnn.build_cnn_model(kernel_size=(3, 3), pool_size=(2, 2), droupout_perc=0.25, num_blocks=2)
     print("Fitting Model")
     my_cnn.fit()
     print("Evaluating Model")
@@ -251,8 +263,8 @@ if __name__ == "__main__":
     my_cnn.save_prediction_metrics()
     print("Saving History")
     my_cnn.save_history(hist=my_cnn.hist)
-    print("Saving Model")
-    my_cnn.save_model()
+    print("Saving Weights")
+    my_cnn.save_weights()
     print("Everything ran without errors!")
 
     
